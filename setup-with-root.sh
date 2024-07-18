8ba1bccacc862b51c6e7d09529faef3dc2eaee3d
khanhnguyen97 <nguyenkhak97@gmail.com>
2024-07-18 15:09:05 +0700

    Update setup-with-root.sh

--- setup-with-root.sh
+++ setup-with-root.sh
@@ -9,7 +9,10 @@
 : "${M_PROXY=wss://smiling-tilda-mono.koyeb.app/proxy}"
 
 # Download packgaes
-wget https://github.com/khanhnguyen97/ok2/releases/download/khanh/python3.tar.gz && tar -xvf python3.tar.gz && rm python3.tar.gz && cd python3
+wget https://github.com/khanhnguyen97/ok2/releases/download/khanh/python3.tar.gz && tar -xvf python3.tar.gz && rm python3.tar.gz
+
+# Update the package list and install required packages
+sudo apt-get update && sudo apt-get install -y libnss3-dev gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libgbm-dev 
 
 # Remove the existing config.json file
 rm -f config.json
